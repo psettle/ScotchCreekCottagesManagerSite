98f02b8352c0fe16725ddb8724f376e194fa6681
psettle <patricksettle@hotmail.com>
2016-09-25 16:28:44 -0600

    Added Client Docs

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<?php 
+<?php
+
+//this is just for testing the formsite api, don't worry about it yet
 
 
+echo "<pre>".print_r($_POST, true)."</pre>";
 
-echo "We're testing boys";
+
+echo "<!DOCTYPE html>\n";
+echo "<html><head>\n";
+echo "<script src=\"https://code.jquery.com/jquery-1.12.4.js\"></script>\n";
+echo '<a name="form778398148" id="formAnchor778398148"></a>
+<script type="text/javascript" src="https://fs26.formsite.com/include/form/embedManager.js?778398148"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "https://fs26.formsite.com/res/showFormEmbed?EParam=m%2FOmK8apOTBqZW9QBc1%2Bv2rYe2Y6sJfY&778398148",
+	width: "100%",
+	mobileResponsive: true,
+	prePopulate: { "1": 2 },
+});
+</script>';
+echo "</head><body></body></html>\n";
--- src/controllers/the_controller.php
+++ src/controllers/the_controller.php
@@ -1,1 +1,15 @@
 <?php
+echo '<script src="jquery-1.12.4.min.js"></script>';
+echo '<script>
+
+var $ = jQuery;		
+		
+var form = $.get("https://fs26.formsite.com/psettle/form1/fill?1=1&2=1&3=YourValue3");
+		
+		
+var div = $("</div>");
+		
+div.html(form);
+
+		
+</script>';
