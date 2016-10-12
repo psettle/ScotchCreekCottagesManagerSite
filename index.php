8f75abceb5d5ed3795974ee7ab06be906b68b3b3
psettle <patricksettle@hotmail.com>
2016-10-12 16:04:57 -0600

    Added phalcon bootstrapper

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
+
+
 <?php
 
-//this is just for testing the formsite api, don't worry about it yet
-
-
-echo "<pre>".print_r($_POST, true)."</pre>";
-
-
+/*
 echo "<!DOCTYPE html>\n";
 echo "<html><head>\n";
 echo "<script src=\"https://code.jquery.com/jquery-1.12.4.js\"></script>\n";
@@ -19,4 +16,83 @@ EmbedManager.embed({
 	prePopulate: { "1": 2 },
 });
 </script>';
-echo "</head><body></body></html>\n";
+echo "</head><body></body></html>\n";*/
+
+
+use Phalcon\Loader;
+use Phalcon\Mvc\Application;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+
+//this initializes phalcon and loads it's classes
+
+$loader = new Loader();
+
+$loader->registerDirs (array(
+		'controllers/',
+		'models/',
+));
+
+$loader->register();
+
+//next we specify which phalcon services should be initializes (DB, login, etc, this handles the common ones)
+
+$di = new FactoryDefault();
+
+//now we add the views that will be a part of our app
+$di->set (
+		'view',
+		function() {
+			$view = new View();
+			$view->setViewsDir('views/');
+			return $view;
+		}
+	);
+
+
+
+$di->set(
+		'url',
+		function() {
+			$url = new UrlProvider;
+			$url->setBaseUri("/");
+			return $url;
+		}
+	);
+
+
+
+
+$di->set(
+		'db',
+		function() {
+			return new DbAdapter(array(
+					'host' => '127.0.0.1',
+					'username' => 'root',
+					'password' => 'abc123',
+					'dbname' => 'phalcon_db'
+
+			));
+		}
+	);
+
+
+//finally we create an application with the settings we used
+$application = new Application($di);
+
+try {
+
+	$response = $application->handle();
+
+	$response->send();
+
+} catch (Exception $e) {
+	echo "Error: ". $e->getMessage();
+}
+
+
+
+
+
